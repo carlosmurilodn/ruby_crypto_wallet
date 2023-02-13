9663df70719d45280335a709b19c772ecaaa5ab1
carlosmurilodn <carlos05111991@hotmail.com>
2023-02-13 13:20:52 -0300

    Segundo commit

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -39,7 +39,7 @@ class CoinsController < ApplicationController
   def update
     respond_to do |format|
       if @coin.update(coin_params)
-        format.html { redirect_to coin_url(@coin), notice: "Coin was successfully updated." }
+        format.html { redirect_to coin_url(@coin), notice: "A moeda foi excluida com sucesso." }
         format.json { render :show, status: :ok, location: @coin }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class CoinsController < ApplicationController
     @coin.destroy
 
     respond_to do |format|
-      format.html { redirect_to coins_url, notice: "Coin was successfully destroyed." }
+      format.html { redirect_to coins_url, notice: "A moeda foi excluida com sucesso." }
       format.json { head :no_content }
     end
   end
