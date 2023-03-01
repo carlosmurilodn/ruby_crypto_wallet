2afbbb0c3d2ca4500714efa322606e41d23c5361
carlosmurilodn <carlos05111991@hotmail.com>
2023-03-01 14:07:50 -0300

    Usando a aplicaçao vários idiomas em tempo de execuçao

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires[COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails - Jackson Pires[SESSION]"
     @meu_nome = params[:nome]
     @nome_curso = params[:curso]
   end
