fbeb8d50ebb71c213682c74cfab77e645300e24e
carlosmurilodn <carlos05111991@hotmail.com>
2023-03-23 17:15:09 -0300

    dark mode

--- app/javascript/welcome.js
+++ app/javascript/welcome.js
@@ -1,2 +1,10 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+const corpo = document.body
+
+corpo.onclick = ()=>{
+}
+
+alert("dgfgdfgd")
